17813c2edd364577f3505e2b4663229bc5ec4c9c
wanyang <wanyang@broada.com>
2021-04-18 22:28:19 +0800

    add start

---
+++ start/src/main/java/com/github/oneyoung/start/Application.java
@@ -0,0 +1,18 @@
+package com.github.oneyoung.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application
+ *
+ * @author oneyoung
+ * @since 2021/4/18 018 22:12
+ */
+
+@SpringBootApplication(scanBasePackages = {"com.github.oneyoung"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
