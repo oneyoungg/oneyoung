7beabca130057a4b182aaf3706db23b3063c9ecb
yang.wan <wan.yang@iwhalecloud.com>
2023-08-25 14:32:07 +0800

    add

---
+++ oneyoung-base/src/main/java/top/oneyoung/base/concurrent/RTest.java
@@ -0,0 +1,15 @@
+package top.oneyoung.base.concurrent;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * RenchLock
+ *
+ * @author oneyoung
+ * @since 2023/8/24 17:51
+ */
+public class RTest {
+    public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock();
+    }
+}
